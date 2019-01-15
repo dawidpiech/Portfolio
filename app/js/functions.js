2943aac05c3a41a503a1e85bfba31f75cd5728ae
Dawid Piech <dawid.piech243@gmail.com>
2019-01-15 17:34:51 +0100

    Create my portfolio

---
+++ app/js/functions.js
@@ -0,0 +1,321 @@
+function changeActiveBar() {
+    let a = this.dataset.bar,
+        bar_desc = document.querySelectorAll(".bar-description"),
+        bar_active = document.querySelector(".bar-descriptions .active")
+
+    bar_active.classList.remove("active")
+    bar_desc[a - 1].classList.add("active")
+}
+
+function loop(a) {
+
+    let slides = document.querySelectorAll(".project"),
+        slider = document.querySelector(".projects-container"),
+        projects_container = document.querySelector(".projects")
+
+    if (a) {
+        carousel -= projects_container.clientWidth / slides.length
+        arrow_left.style.visibility = "visible"
+        if (Math.abs(carousel) + slider.clientWidth > projects_container.clientWidth) {
+            carousel = 0;
+            arrow_left.style.visibility = "hidden"
+        }
+        projects_container.style.transform = `translateX(${carousel}px)`
+    }
+    else {
+        carousel += projects_container.clientWidth / slides.length
+        projects_container.style.transform = `translateX(${carousel}px)`
+        if (carousel <= 0)
+            arrow_left.style.visibility = "hidden"
+    }
+}
+
+function initMap() {
+    let styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#212121"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#212121"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.country",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#bdbdbd"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#181818"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#1b1b1b"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#2c2c2c"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#8a8a8a"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#373737"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#3c3c3c"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#4e4e4e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#3d3d3d"
+                    }
+                ]
+            }
+        ]
+    )
+
+    let map = new google.maps.Map(document.querySelector('.map'), {
+        center: { lat: 50.081596725673236, lng: 19.949696344901934 },
+        zoom: 15,
+        disableDefaultUI: true
+    }),
+
+        marker = new google.maps.Marker({
+            position: { lat: 50.081596725673236, lng: 19.949696344901934 },
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: {
+                url: "../assets/img/marker.png",
+                scaledSize: new google.maps.Size(60, 72)
+            }
+        })
+
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+}
+
+function validate(name, mail, message) {
+    let controll = true
+    if (name.value === "") {
+        name.parentElement.style.borderColor = "#F00"
+        controll = false
+    }
+    else {
+        name.parentElement.style.removeProperty("border-color")
+    }
+    if (validateMail(mail)) {
+        mail.parentElement.style.borderColor = "#F00"
+        controll = false
+    }
+    else {
+        mail.parentElement.style.removeProperty("border-color")
+    }
+    if (message.value === "") {
+        message.parentElement.style.borderColor = "#F00"
+        controll = false
+    }
+    else {
+        message.parentElement.style.removeProperty("border-color")
+    }
+
+    return controll
+}
+
+function validateMail(a) {
+    let controll = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(a.value)) ? false : true
+    return controll
+}
+
+function sendMessage(name, firm, mail, phone, message) {
+    if (validate(name, mail, message)) {
+        Email.send({
+            SecureToken: "d79c6744-5db7-487e-ba33-0ae13c93cd26",
+            To: 'dawid@piech.it',
+            From: "portfolio@piech.it",
+            Subject: `Name: ${name}, Firm: ${firm}, Phone: ${phone}, Message: ${message}`,
+            Body: message
+        }).then(
+            message => alert(message)
+        )
+        name.parentElement.style.removeProperty("border-color")
+        mail.parentElement.style.removeProperty("border-color")
+        message.parentElement.style.removeProperty("border-color")
+    }
+}
+
+function checkImagesLoaded(imagesArray, preloaderHandle) {
+    let loaded = imagesArray.length
+
+    for (let i = 0; i < imagesArray.length; i++) {
+        if (imagesArray[i].complete) {
+            loaded--
+        }
+        else {
+            imagesArray[i].addEventListener("load", () => {
+                loaded--
+                if (loaded === 0) {
+                    allImagesLoaded(preloaderHandle)
+                }
+            })
+        }
+    }
+
+    if (loaded === 0){
+        allImagesLoaded(preloaderHandle)
+    }
+}
+
+function allImagesLoaded(preloader){
+    document.querySelector("body").classList.remove("load")
+    preloader.classList.add("isLoaded")
+}
+
+function paralax(){
+    document.querySelector(".section-header").style.backgroundPositionY = window.pageYOffset/4 + "px"
+}
---
+++ app/js/script.js
@@ -0,0 +1,109 @@
+let hamburger = document.querySelector(".hamburger"),
+    menu_fields = document.querySelectorAll(".menu-form-field input"),
+    mail_area = document.querySelectorAll(".menu-form-field textarea"),
+    black_area = document.querySelector(".menu-back"),
+    bars = document.querySelectorAll(".bar"),
+    arrow_left = document.querySelector(".arrow-left"),
+    arrow_right = document.querySelector(".arrow-right"),
+    carousel = 0,
+    send_menu = document.querySelector(".menu-form .send"),
+    send_contact = document.querySelector(".section-form .send"),
+    preloader = document.querySelector(".preloader"),
+    img = document.querySelectorAll("img")
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    
+    //hamburger
+    hamburger.addEventListener("click", () => {
+        document.querySelector("header").classList.toggle("open")
+    })
+
+    //label animation
+    menu_fields.forEach(a => {
+        a.addEventListener("focusin", () => {
+            a.parentElement.classList.add("focus")
+        })
+    })
+
+    mail_area.forEach(a => {
+        a.addEventListener("focusin", () => {
+            a.parentElement.classList.add("focus-area")
+        })
+    })
+
+    menu_fields.forEach(a => {
+        a.addEventListener("focusout", () => {
+            if (a.value === "")
+                a.parentElement.classList.remove("focus")
+        })
+    })
+
+
+    //label form animation
+    mail_area.forEach(a => {
+        a.addEventListener("focusout", () => {
+            if (a.value === "")
+                a.parentElement.classList.remove("focus-area")
+        })
+    })
+
+
+    //close menu
+    black_area.addEventListener("click", () => {
+        document.querySelector("header").classList.remove("open")
+    })
+
+    for (let i = 0; i < bars.length; i++) {
+        bars[i].addEventListener("click", changeActiveBar)
+    }
+
+    try {
+        //Projects carousel
+        arrow_left.addEventListener("click", () => {
+            loop(false)
+        })
+        arrow_right.addEventListener("click", () => {
+            loop(true)
+        })
+    }
+    catch{
+
+    }
+
+    try {
+
+        //Contact form
+        send_contact.addEventListener("click", function () {
+            let contact_form = [
+                document.querySelector(".section-form input[name='name']"),
+                document.querySelector(".section-form input[name='company_name']"),
+                document.querySelector(".section-form input[name='e-mail']"),
+                document.querySelector(".section-form input[name='telephone']"),
+                document.querySelector(".section-form textarea[name='message']")
+            ]
+            sendMessage(...contact_form)
+        })
+
+        send_menu.addEventListener("click", function () {
+            let menu_form = [
+                document.querySelector(".menu-form input[name='name']"),
+                document.querySelector(".menu-form input[name='company_name']"),
+                document.querySelector(".menu-form input[name='e-mail']"),
+                document.querySelector(".menu-form input[name='telephone']"),
+                document.querySelector(".menu-form textarea[name='message']")
+            ]
+
+            sendMessage(...menu_form)
+        })
+    }
+    catch{
+
+    }
+
+    setTimeout(() => {
+        checkImagesLoaded(img, preloader)
+    }, 500);
+
+    window.addEventListener("scroll", paralax)
+})
